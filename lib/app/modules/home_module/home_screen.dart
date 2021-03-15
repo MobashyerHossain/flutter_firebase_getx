054f354269ccacd29b8e4b89caf8f28526deea87
jokerjamil007@gmail.com <jokerjamil007@gmail.com>
2021-03-15 15:09:46 +0600

    login & signing module initialized

--- lib/app/modules/home_module/home_screen.dart
+++ lib/app/modules/home_module/home_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
+  const HomeScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
